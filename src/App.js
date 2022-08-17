7cec9949eab735fc4147c8cd004791627eb0ff8f
FrancisShiangah <enochdaywalker@gmail.com>
2022-08-17 15:21:53 +0000

    ui specs

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Header from './hearder'
+import ShopCards from './shopsCard'
+
+class App extends React.Component{
+    constructor(){
+        super()
+        this.state= {
+            name: "shanghai",
+            age: 34
+        }
+    }
+    render(){
+        return(
+            <div>
+                
+            </div>
+            
+        )
+    }
+}
+
+
+export default App
---
+++ src/shopsCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ShopCards(){
+    return(
+        <div>
+            <b>Shop Name</b>
+            <p>Location</p>
+            <p>rating</p>
+            <p>booked appointments</p>
+        </div>
+    )
+}
+
+export default ShopCards;
